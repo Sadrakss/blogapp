36c76b57e9ad7ab36cbd3ddaa56f724022d99b57
sadrakss <sadrakss@pop-os.localdomain>
2020-01-22 09:53:20 -0300

    saving post in bank

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,13 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/category')
 const category = mongoose.model('categories')
+require('../models/post')
+const post = mongoose.model('posts')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
 })
 
-router.get('/posts', (req, res) => {
-    res.send('Posts page')
-})
 router.get('/categories', (req, res) => {
     category.find().sort({ date: 'desc' }).then((categories) => {
         res.render('admin/categories', { categories: categories })
@@ -69,7 +68,7 @@ router.post('/categories/edit', (req, res) => {
         category.save().then(() => {
             req.flash('success_msg', 'successfully edited category')
             res.redirect('/admin/categories')
-        }).catch((err)=>{
+        }).catch((err) => {
             req.flash('error_msg', 'there was an internal error saving category edit ')
             res.redirect('/admin/categories')
         })
@@ -80,14 +79,53 @@ router.post('/categories/edit', (req, res) => {
     })
 })
 
-router.post('/categories/delete',(req,res)=>{
-    category.findOneAndRemove({_id:req.body.id}).then(()=>{
+router.post('/categories/delete', (req, res) => {
+    category.findOneAndRemove({ _id: req.body.id }).then(() => {
         req.flash('success_msg', 'category successfully deleted')
         res.redirect('/admin/categories')
-    }).catch((err)=>{
+    }).catch((err) => {
         req.flash('error_msg', 'there was an error deleting the category')
         res.redirect('/admin/categories')
     })
+})
+
+router.get('/posts', (req, res) => {
+    res.render('admin/posts')
+})
+router.get('/posts/add', (req, res) => {
+    category.find().then((categories) => {
+        res.render('admin/addpost', { categories: categories })
+    }).catch((err) => {
+        req.flash('error_msg', 'there was an error loading the form')
+        res.redirect('/admin')
+    })
+})
+
+router.post('/posts/new', (req, res) => {
+    var error=[]
+
+    if(req.body.category == '0'){
+        error.push({text:"invalid category, register a category!"})
+    }
+    if(error.length > 0){
+        res.render('admin/addpost',{error:error})
+    }else{
+        const newPost={
+            title:req.body.title,
+            slug:req.body.slug,
+            description:req.body.description,
+            content:req.body.content,
+            category:req.body.category
+        }
+
+        new post(newPost).save().then(()=>{
+            req.flash('success_msg', 'post created successfully')
+            res.redirect('/admin/posts')
+        }).catch((err)=>{
+            req.flash('error_msg','there was an error saving the post')
+            res.redirect('/admin/posts')
+        })
+    }
 })
 
 module.exports = router
