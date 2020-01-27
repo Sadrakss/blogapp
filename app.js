2b9664a23b1fdf239f7d3cb6cc732fe15a5b8105
sadrakss <sadrakss@pop-os.localdomain>
2020-01-27 16:48:25 -0300

    a

--- app.js
+++ app.js
@@ -54,7 +54,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
 app.get('/', (req, res) => {
-    post.find().populate('category').sort({ date: 'desc' }).then((posts) => {
+    post.find()
+    .populate('category')
+    .sort({ date: 'desc' })
+    .then((posts) => {
         res.render('index', { posts: posts })
     }).catch((err) => {
         req.flash('error_msg', 'There was an internal error')
